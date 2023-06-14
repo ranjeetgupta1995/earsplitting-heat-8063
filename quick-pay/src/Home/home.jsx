a48e0ddb924bf6c4ff40729ba3cee2647cb2c54b
ranjeetgupa114@gamil.com <you@example.com>
2023-06-14 23:03:53 +0530

    day-1

---
+++ quick-pay/src/Home/home.jsx
@@ -0,0 +1,38 @@
+function Home () {
+    return(
+        <>
+        <div>
+            <img style={{width:"100%"}} src="https://miro.medium.com/v2/resize:fit:828/format:webp/1*OpfMYaiBsYHRxPdAcLeYrw.png" alt="" />
+        </div>
+        <div class="container" style={{display:"flex"}}>
+        <div style={{backgroundColor:"#0D47A1", color:"white", width:"50%"}}>
+            <div>
+                <h1>Simple, Fast & Secure</h1>
+            </div>
+            <hr></hr>
+            <div>
+                <div>
+                    <h2>One app for all things money.</h2>
+                    <p>Pay bills, recharge, send money, buy gold, invest and shop at your favourite stores.</p>
+                </div>
+                <hr></hr>
+                <div>
+                    <h2>Pay whenever you like, wherever you like.</h2>
+                    <p>Choose from options like UPI, the PhonePe wallet or your Debit and Credit Card.</p>
+                </div>
+                <hr></hr>
+                <div>
+                    <h2>Find all your favourite apps on PhonePe Switch.</h2>
+                    <p>Book flights, order food or buy groceries. Use all your favourite apps without downloading them.</p>
+                </div>
+            </div>
+        </div>
+        <div style={{width:"50%"}}>
+            <img style={{width:"100%"}} src="https://assets.ey.com/content/dam/ey-sites/ey-com/en_in/topics/cybersecurity/2022/12/ey-digital-payment.jpg" alt="" />
+        </div>
+        </div>
+        </>
+    )
+};
+
+export default Home;
